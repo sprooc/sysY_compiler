01b16afdb2f436ac5855d9c7ffb4ca4f17ad2130
221250127-Zhang Junbin <1127626033@qq.com>
2024-05-22 12:27:06 +0800

    run

--- src/backend/MenAllocator.cpp
+++ src/backend/MenAllocator.cpp
@@ -24,9 +24,8 @@ void MenAllocator::alloc(string name, Type* type) {
 Type* MenAllocator::getType(string name) {
   auto it = type_table->find(name);
   if (it == type_table->end()) {
-    return nullptr;
-    // std::cerr << "Cannot find type of " << name << std::endl;
-    // exit(1);
+    std::cerr << "Cannot find type of " << name << std::endl;
+    exit(1);
   }
   return it->second;
 }
